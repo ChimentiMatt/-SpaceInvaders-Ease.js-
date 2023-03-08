f7da3cc229b7b1fa698eab6f516f2f9b1f020a2c
ChimentiMatt <chimcode@gmail.com>
2023-03-07 18:39:41 -0800

    tweak on enemyBullet falling speed to make them universal

--- src/constructors/EnemyBullet.js
+++ src/constructors/EnemyBullet.js
@@ -1,6 +1,8 @@
 class EnemyBullet {
     constructor(spriteSheet){
         this.bullet = new createjs.Sprite(spriteSheet, "default");
+        this.x;
+        this.y
     }
   
     addToStage = function(stage, invaders) {
@@ -21,10 +23,15 @@ class EnemyBullet {
             let randomIndex = Math.floor(Math.random() * (max - min) + min)
     
             let currentInvader = invaders[aliveInvaders[randomIndex]]
-    
+            
+            // current location being set
             this.bullet.x =  currentInvader.x;
             this.bullet.y = currentInvader.y;
-    
+            
+            // for future reference
+            this.x = currentInvader.x
+            this.y = currentInvader.y;
+
             stage.addChild(this.bullet)
         }
         
@@ -41,10 +48,12 @@ class EnemyBullet {
         let playerLocation = {x: players[0].x, y: players[0].y}
         let xVariance = Math.floor(Math.random() * (stage.canvas.width - 0) + 0)
         let durationVariance = Math.floor(Math.random() * (3000 - 2000) + 2000)
-        
 
-        createjs.Tween.get(this.bullet )
-        .to( { y: playerLocation.y + 4  + 150}, durationVariance)
+        durationVariance = 2000
+        // y: playerLocation.y + 4  + 150
+
+        createjs.Tween.get(this.bullet)
+        .to( { y: this.y + 500}, durationVariance)
         .call(() => {
             stage.removeChild(this.bullet);
             createjs.Tween.removeTweens(this.bullet)
